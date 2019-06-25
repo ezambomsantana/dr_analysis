9734c677457c1592bb6f8cf4b72b85e204b07878
Eduardo Santana <ezambomsantana@gmail.com>
2019-06-25 10:07:31 -0300

    base

--- scenario_base/main.py
+++ scenario_base/main.py
@@ -9,15 +9,26 @@ from sklearn.linear_model import LinearRegression
 
 events = pd.read_csv('events.csv', sep = ';', header=None, names=['real_hour', 'real_minute', 'time','car', 'final_link', 'total_time','total_distance'])
 
+events['real_minute'] = pd.to_numeric(events['real_minute'])
+events['total_time'] = pd.to_numeric(events['total_time'])
+events['inicio'] = events['real_minute'] - events['total_time']
+
+count_cars = []
+value = 200
+for x in range(1, 25):
+    inte = value * x
+    count = 0
+    for index, row in events.iterrows():
+        if row['inicio'] < inte and row['total_time'] + row['inicio'] > inte:
+            count = count + 1
+    count_cars.append([inte, count])
+
 bins = []
 names = []
-
 bins.append(0)
 for x in range(1, 50):
     bins.append(x * 100)
     names.append(str((x-1)*100))
-
-print(events)
     
 events['interval'] = pd.cut(events['real_minute'], bins, labels=names)
 
@@ -28,13 +39,16 @@ events_mean = events_mean[['interval', 'total_time']].dropna()
 events_mean.plot.line(x='interval', y='total_time')
 plt.show()
 
+par = events[events['car'].str.contains("paraiso")]
+cons = events[events['car'].str.contains("consolacao")]
 
-X = events_mean.iloc[:, 0].values.reshape(-1, 1)  # values converts it into a numpy array
-Y = events_mean.iloc[:, 1].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
-linear_regressor = LinearRegression()  # create object for the class
-linear_regressor.fit(X, Y)  # perform linear regression
-Y_pred = linear_regressor.predict(X)  # make predictions
+print(par['interval'].size)
+print(cons['interval'].size)
 
-plt.scatter(X, Y)
-plt.plot(X, Y_pred, color='red')
+par['interval'] = pd.to_numeric(par['interval'])
+par.plot.scatter(x='interval', y='total_time')
+plt.show()
+
+cons['interval'] = pd.to_numeric(cons['interval'])
+cons.plot.scatter(x='interval', y='total_time')
 plt.show()

8881a2fb199b24ed00af58b2b86bf0e4cc4bfe1f
Eduardo Santana <ezambomsantana@gmail.com>
2019-07-12 16:39:11 -0300

    add-individual-analysis

--- main2.py
+++ main2.py
@@ -10,24 +10,105 @@ from sklearn.linear_model import LinearRegression
 data2 = []
 data3 = []
 
+data_dr2 = []
+data_dr3 = []
+
+data_non_dr2 = []
+data_non_dr3 = []
+
 for x in range(0,21):
     file1 = 'scenario2/volume' + str(x) + '/events.xml'
     events = pd.read_csv(file1, sep = ';', header=None, names=['real_hour', 'real_minute', 'time','car', 'final_link', 'total_time','total_distance'])
-
-    print(events['total_time'].mean())
-
     data2.append([x * 5, events['total_time'].mean(), events['total_time'].std()/10])
+
+    events_dr = events[events['car'].str.contains("dr")] 
+    events_non_dr = events[~events['car'].str.contains("dr")] 
+    
+    data_dr2.append([x * 5, events_dr['total_time'].mean(), events_dr['total_time'].std()/10])
+    data_non_dr2.append([x * 5, events_non_dr['total_time'].mean(), events_non_dr['total_time'].std()/10])
+
+
 
 for x in range(0,21):
     file1 = 'scenario3/volume' + str(x) + '/events.xml'
     events = pd.read_csv(file1, sep = ';', header=None, names=['real_hour', 'real_minute', 'time','car', 'final_link', 'total_time','total_distance'])
-
-    print(events['total_time'].mean())
-
     data3.append([x * 5, events['total_time'].mean(), events['total_time'].std()/10])    
+
+    events_dr = events[events['car'].str.contains("dr")] 
+    events_non_dr = events[~events['car'].str.contains("dr")] 
+    
+    data_dr3.append([x * 5, events_dr['total_time'].mean(), events_dr['total_time'].std()/10])
+    data_non_dr3.append([x * 5, events_non_dr['total_time'].mean(), events_non_dr['total_time'].std()/10])
 
 df2 = pd.DataFrame(data2, columns = ['scenario', 'mean_travel', 'std_travel']) 
 df3 = pd.DataFrame(data3, columns = ['scenario', 'mean_travel', 'std_travel']) 
+
+ax = df2.plot(
+    x='scenario',
+    y='mean_travel',
+    marker='o', 
+    yerr='std_travel',
+    ylim=(0, None),
+    title='Travel time (s) vs. DR ratio (%)',
+    grid=True,
+)
+
+ax = df3.plot(ax=ax,
+    x='scenario',
+    y='mean_travel',
+    marker='o', 
+    yerr='std_travel',
+    ylim=(0, None),
+    title='Travel time (s) vs. DR ratio (%)',
+    grid=True,
+)
+
+cet_mean = 662
+cet_std = 25
+
+ax.hlines(y=cet_mean, xmin=0, xmax=100)
+ax.hlines(y=[cet_mean+2*cet_std, cet_mean-2*cet_std], xmin=0, xmax=100, linestyle='--')
+
+ax.legend(['Mean, no DR', '95% CI, no DR', 'Mean, Scenario 2', 'Mean Scenario 3'])
+plt.show()
+
+
+df2 = pd.DataFrame(data_dr2, columns = ['scenario', 'mean_travel', 'std_travel']) 
+df3 = pd.DataFrame(data_dr3, columns = ['scenario', 'mean_travel', 'std_travel']) 
+
+ax = df2.plot(
+    x='scenario',
+    y='mean_travel',
+    marker='o', 
+    yerr='std_travel',
+    ylim=(0, None),
+    title='Travel time (s) vs. DR ratio (%)',
+    grid=True,
+)
+
+ax = df3.plot(ax=ax,
+    x='scenario',
+    y='mean_travel',
+    marker='o', 
+    yerr='std_travel',
+    ylim=(0, None),
+    title='Travel time (s) vs. DR ratio (%)',
+    grid=True,
+)
+
+cet_mean = 662
+cet_std = 25
+
+ax.hlines(y=cet_mean, xmin=0, xmax=100)
+ax.hlines(y=[cet_mean+2*cet_std, cet_mean-2*cet_std], xmin=0, xmax=100, linestyle='--')
+
+ax.legend(['Mean, no DR', '95% CI, no DR', 'Mean, Scenario 2', 'Mean Scenario 3'])
+plt.show()
+
+
+
+df2 = pd.DataFrame(data_non_dr2, columns = ['scenario', 'mean_travel', 'std_travel']) 
+df3 = pd.DataFrame(data_non_dr3, columns = ['scenario', 'mean_travel', 'std_travel']) 
 
 ax = df2.plot(
     x='scenario',
